9761d12a146a583efb8eb1bf725bf3975d07d83d
Annabelle Perbet <annabelleperbet@yahoo.fr>
2025-09-11 16:59:22 +0200

    Ajout du fichier script.js contenant la fonction getCurrentYear

---
+++ script.js
@@ -0,0 +1,19 @@
+// Récupération des éléments HTML
+const footerCopyrightYear = document.querySelector(".footer__text__year");
+const circles = document.querySelectorAll(".circle");
+
+// Déclaration de la fonction getCurrentYear qui va permettre l'affichage dynamique de l'année
+const getCurrentYear = () => {
+  // Récupération de la date actuelle stockée dans la constante date
+  const date = new Date();
+  //console.log(date);
+
+  // Récupération de l'année stockée dans la constante year
+  const year = date.getFullYear();
+  //console.log(year);
+
+  // Affichage dynamique de l'année en cours
+  footerCopyrightYear.textContent = `${year}`;
+};
+// Appel de la fonction getCurrentYear()
+getCurrentYear();
